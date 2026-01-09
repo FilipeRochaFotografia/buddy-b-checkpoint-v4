2ac1dfafe674e00b889f51ed5e10bcbc11270f8b
Owner 0054 <FilipeRochaFotografia@gmail.com>
2026-01-09 13:01:52 -0300

    Em breve 2

--- src/sections/product/DownloadApp.tsx
+++ src/sections/product/DownloadApp.tsx
@@ -66,7 +66,7 @@ export function DownloadApp() {
                       />
                     </a>
                   </div>
-                  <span className="text-[10px] text-[#424242] italic pr-1">*Em breve</span>
+                  <span className="text-[10px] text-[#424242] italic pr-1">*Em breve na App Store</span>
                </div>
              </div>
 
@@ -83,7 +83,7 @@ export function DownloadApp() {
                     />
                   </a>
                 </div>
-                <span className="text-[10px] text-[#424242] italic w-full text-right mt-1 pr-2">*Em breve na App Store</span>
+                <span className="text-[10px] text-[#424242] italic w-full text-right mt-1 pr-2">*Em breve</span>
              </div>
 
           </motion.div>
